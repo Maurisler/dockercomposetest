e75dde2d2aafa205c3af7ff0a28c1478897f1cd0
Maurin Isler <71018564+Maurisler@users.noreply.github.com>
2023-02-24 23:18:39 +0100

    Update Main.java

--- programm/Main.java
+++ programm/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Collections;
 
-public class Addressbook {
+public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String inputName =  "null";
